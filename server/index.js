c98a6f4f2a20004ea65f8e6ae9fc55546c3fe75d
veejask-41 <sajeevk2001@gmail.com>
2023-09-24 23:11:02 +0530

    get and post requests

--- server/index.js
+++ server/index.js
@@ -1,7 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.use(express.json());
+
 const db = require("./models");
+
+// Routes
+const postsRouter = require("./routes/Posts");
+app.use("/posts", postsRouter);
 
 db.sequelize.sync().then(() => {
   app.listen(1234, () => {
